6776ab1036e89aadc18bcfa913bd8aa4bb17ffcc
juradrugiPosao <drazovko@foi.hr>
2017-09-24 19:13:16 +0200

    2. snimanje

--- ConsoleApplication3_3/ConsoleApplication3_3/ConsoleApplication3_3.cpp
+++ ConsoleApplication3_3/ConsoleApplication3_3/ConsoleApplication3_3.cpp
@@ -2,10 +2,68 @@
 //
 
 #include "stdafx.h"
-
+#include "iostream"
+#include <fstream>
+using namespace std;
 
 int main()
 {
+	constexpr int nmax = 100;
+
+	ifstream ulazniTok("brojevi.dat");
+	if (!ulazniTok)
+	{
+		cerr << "Ne mogu otvoriti traženu datoteku" << endl;
+		return 1;
+	}
+
+	double brojevi[nmax];
+	int n = 0;
+	double broj;
+
+	while (ulazniTok >> broj)
+	{
+		if (n >= nmax)
+		{
+			cerr << "Previše podataka!" << endl;
+			return 2;
+		}
+		brojevi[n++] = broj;
+	}
+
+	if (n==0)
+	{
+		cerr << "Nema podataka!" << endl;
+		return 3;
+	}
+
+	for (int i = n - 1; i > 0; --i)
+	{
+		bool zamjena = false;
+		for (size_t j = 0; j < n; j++)
+		{
+			if (brojevi[j+1] < brojevi[j])
+			{
+				auto privremeni = brojevi[j];
+				brojevi[j] = brojevi[j + 1];
+				brojevi[j + 1] = privremeni;
+				zamjena = true;
+			}
+		}
+		if (zamjena == false)
+		{
+			break;
+		}
+	}
+
+	cout << "Brojevi slozeni po velicini: " << endl;
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << brojevi[i] << endl;
+	}
+
+	system("pause");
+
     return 0;
 }
 
